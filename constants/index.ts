47173d93617576a1c9a1ff1ef4866a3925c49d18
svartse <killbillbeatrixx@gmail.com>
2023-08-23 22:20:18 +0300

    components

---
+++ constants/index.ts
@@ -0,0 +1,26 @@
+export const navLinks = [
+    {
+        id: "leads",
+        title: "Leads",
+    },
+    {
+        id: "warmup",
+        title: "Warmup",
+    },
+    {
+        id: "pricing",
+        title: "Pricing",
+    },
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "reviews",
+        title: "Reviews",
+    },
+    {
+        id: "blog",
+        title: "Blog",
+    },
+];
