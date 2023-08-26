d32c21e6cca4d25fb7a302481e74b5ce131a58a1
svartse <killbillbeatrixx@gmail.com>
2023-08-26 23:04:27 +0300

    AccordionCards

--- components/AccordionCards.tsx
+++ components/AccordionCards.tsx
@@ -2,6 +2,7 @@
 import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { AccordionProps } from '@/types';
+import CustomButton from './CustomButton';
 
 
 interface AccordionCardProps {
@@ -18,6 +19,12 @@ const AccordionCards = ({ accordionCards }: AccordionCardProps) => {
             <AccordionDetails>
                 <Typography className='text-white'>{content}</Typography>
                 <img src={img} alt="card" />
+                <div className="flex justify-center mb-10 mt-10">
+                    <CustomButton
+                        title="Start For Free"
+                        containerStyles="uppercase text-white text-lg bg-gradient-to-r from-cyan-300 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 rounded-3xl text-center"
+                    />
+                </div>
             </AccordionDetails>
         </Accordion>
     )
--- components/CampaignAnalytics.tsx
+++ components/CampaignAnalytics.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import CustomButton from './CustomButton';
 
 const CampaignAnalytics = () => {
     return (
@@ -16,7 +17,13 @@ const CampaignAnalytics = () => {
                 <p className="text-white text-center ">
                     See exactly what’s working with our advanced analytics dashboard. <br /> Pause the campaigns that need work and <br /> scale the ones that are performing well.
                 </p>
-            </div>
+                <div className="flex justify-center mb-10 mt-10">
+                    <CustomButton
+                        title="Start For Free"
+                        containerStyles="uppercase text-white text-lg bg-gradient-to-r from-indigo-300 via-indigo-500 to-indigo-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:focus:ring-indigo-800 shadow-lg shadow-indigo-500/50 dark:shadow-lg dark:shadow-indigo-800/80 rounded-3xl text-center"
+                    />
+                </div>
+            </div>         
         </div>
     )
 }
