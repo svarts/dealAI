78e370dec7f84b6ece14ecca81a460fefcaaab71
svartse <killbillbeatrixx@gmail.com>
2023-08-26 20:34:54 +0300

    CampaignAnalytics

--- components/CampaignAnalytics.tsx
+++ components/CampaignAnalytics.tsx
@@ -2,13 +2,21 @@ import Image from "next/image"
 
 const CampaignAnalytics = () => {
     return (
-        <div>
-            <Image 
-                src="./group01.svg"
+        <div className="flex items-center">
+            <Image
+                src="./campaignanalytics.svg"
                 alt="card"
-                width={300}
-                height={400}
+                width={650}
+                height={800}
             />
+            <div>
+                <h1 className="text-gradient text-center text-6xl">
+                    Optimize with <br /> Campaign Analytics
+                </h1>
+                <p className="text-white text-center ">
+                    See exactly what’s working with our advanced analytics dashboard. <br /> Pause the campaigns that need work and <br /> scale the ones that are performing well.
+                </p>
+            </div>
         </div>
     )
 }
