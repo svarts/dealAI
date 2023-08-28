58fa484aa356239b6bf12e55bf82a1435577935e
svartse <killbillbeatrixx@gmail.com>
2023-08-28 13:58:01 +0300

    GetStarted

--- components/GetStarted.tsx
+++ components/GetStarted.tsx
@@ -3,7 +3,7 @@ import CustomButton from './CustomButton'
 
 const GetStarted = () => {
     return (
-        <div className='w-[1000px] h-[200px] text-white uppercase text-white text-lg bg-gradient-to-r from-indigo-300 via-indigo-500 to-indigo-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:focus:ring-indigo-800 shadow-lg shadow-indigo-500/50 dark:shadow-lg dark:shadow-indigo-800/80 rounded-3xl text-cente'>
+        <div className='w-[1000px] h-[200px] text-white text-lg bg-gradient-to-r from-neutral-700 via-neutral-900 to-neutral-950 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-neutral-700 dark:focus:ring-neutral-950 shadow-lg shadow-neutral-900/50 dark:shadow-lg dark:shadow-neutral-950/80 rounded-3xl text-center border mb-10'>
             <h1 className='text-white'>
                 Learn From The Best in Our Private Community
             </h1>
